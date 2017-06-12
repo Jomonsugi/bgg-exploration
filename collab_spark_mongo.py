da24c9895da4367d22712cf1a4927d3c8b9a12bd
Micah Shanks <micah.shanks@gmail.com>
2017-06-11 19:10:53 -0600

    working collab for predicting one user

--- collab_spark_mongo.py
+++ collab_spark_mongo.py
@@ -14,6 +14,10 @@ from math import sqrt
 from operator import add
 import sys
 import time
+from pyspark.sql.functions import UserDefinedFunction
+from pyspark.sql.types import DoubleType
+from pyspark.sql import Row
+from pyspark.sql.types import *
 
 
 spark = ps.sql.SparkSession.builder \
@@ -26,7 +30,7 @@ sc = spark.sparkContext
 sc.setCheckpointDir('checkpoint/')
 sqlContext = SQLContext(sc)
 
-def mongo_to_rdd():
+def mongo_to_rdd_df():
     df = spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
     ugr_data = df.select('user_id','game_id','rating').repartition(16).cache()
     ugr_df = ugr_data.filter("rating is not null")
@@ -93,8 +97,8 @@ def predict_test_df(df_train, df_val, evaluator):
     bestRank = 0
     bestLambda = -1.0
     bestNumIter = -1
-    df_train = df_train.na.drop()
-    df_val=df_val.na.drop()
+    # df_train = df_train.na.drop()
+    # df_val=df_val.na.drop()
     for rank, lmbda, numIter in itertools.product(ranks, lambdas, numIters):
         als = ALS(rank=rank, maxIter=numIter, regParam=lmbda, numUserBlocks=10, numItemBlocks=10, implicitPrefs=False,
                   alpha=1.0,
@@ -137,26 +141,28 @@ def rdd_predict(ugr_rdd):
 def df_predict(df_test, optimized_model):
     seed = 5L
     rank = 10
-    maxIter = 30
+    numIter = 30
     lmbda = .05
 
-    # (full_train, full_test) = ugr_df.randomSplit([0.8, 0.2], seed=0L)
-    # als = ALS(rank = rank, maxIter = maxIter, regParam = lmbda, userCol = "user_id", itemCol="game_id", seed = seed, ratingCol="rating")
-    # optimized_model = als.fit(full_train)
+    # this code is for a sanity check. I converted all ratings to 0.0 and then use by using the new_test_df to predict below, I could see that predictions are made disregarding the rating row, making the rmse valid
+    # print(df_test.show(10))
+    # name = 'rating'
+    # udf = UserDefinedFunction(lambda x: 'new_value', DoubleType())
+    # new_test_df = df_test.select(*[udf(column).alias(name) if column == name else column for column in df_test.columns])
+    # new_test_df=new_test_df.na.fill(0.0)
+    # print("new_test_df:", new_test_df.show(10))
 
-    # print(full_test.take(3))
     predictions = optimized_model.transform(df_test)
-    # print(full_test.take(3))
+    # print(predictions.take(3))
+
     predictions_drop = predictions.dropna()
-    print(predictions_drop.take(3))
+    # print(predictions_drop.take(3))
     evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating", predictionCol="prediction")
     rmse = evaluator.evaluate(predictions_drop)
     print(rmse)
     print 'RMSE for test data: {}'.format(rmse)
 
-    return optimized_model
-
-def predict_for_user_rdd(ugr_rdd, optimized_model, username="jomonsugi"):
+def predict_for_user_rdd(ugr_rdd, ugr_df, username="jomonsugi"):
     client = MongoClient()
     coll = client.bgg_test.game_comments_test
     try:
@@ -164,19 +170,41 @@ def predict_for_user_rdd(ugr_rdd, optimized_model, username="jomonsugi"):
     except TypeError:
         print("username not found")
         return
-    user_ugr = ugr_rdd.filter(lambda row: row[0] == idn)
-    # print("user_ugr_df")
-    # print(type(user_ugr_df))
-    # print(user_ugr_df.take(1))
+    user_ugr = ugr_rdd.filter(lambda x: x[0] == idn)
     #now get games that the user has not rated
     rated_game_ids_lst = [x["game_id"] for x in list(coll.find({"username": "ahalm"}))]
-    # print(rated_game_ids_lst)
-    user_unrated_games = (ugr_rdd.filter(lambda x: x[1] not in rated_game_ids_lst).map(lambda x: (idn, x[1])))
-    # print("user_unrated_games:", user_unrated_games.take(3))
+
+    # not_rated_rdd_test = ugr_rdd.filter(lambda: x[0] != 19)
+    # print(type(not_rated_rdd_test))
+    print(rated_game_ids_lst)
+    # print("rated_game_ids_lst type:", type(rated_game_ids_lst))
+    user_unrated_games = ugr_rdd.filter(lambda x: x[1] not in rated_game_ids_lst).map(lambda x: (idn, x[1], x[2]))
+    # print("user_unrated_games type:" ,type(user_unrated_games))
+    # print("user_unrated_games:", user_unrated_games.take(2))
+
+    # lst = [(174430, 3.4), (169786, 3.4)]
+    # rdd = sc.parallelize(lst)
+    user_unrated_games = ugr_rdd.filter(lambda x: x[1] not in rated_game_ids_lst).map(lambda x: (idn, int(x[1]), x[2]))
+    print(user_unrated_games.collect())
+    print(type(user_unrated_games))
+
+    schema = StructType( [
+        StructField('user_id', IntegerType()),
+        StructField('game_id', IntegerType()),
+        StructField('rating', DoubleType())
+        ])
+
+    df_test = spark.createDataFrame(user_unrated_games, schema)
+    return df_test
+
+    # df = sqlContext.createDataFrame(user_unrated_games)
+    # df = sqlContext.createDataFrame(user_unrated_games, ['user_id', 'game_id', 'rating'])
+    # print(test.show(1))
+    # print(type(user_unrated_games))
     # print("un-rated games by {}: {}".format(username, user_unrated_games.count()))
-    new_user_recommendations = optimized_model.predictAll(user_unrated_games)
-    print("new_user_recommendations:")
-    print(new_user_recommendations.collect())
+    # new_user_recommendations = optimized_model.predictAll(user_unrated_games)
+    # print("new_user_recommendations:")
+    # print(new_user_recommendations.collect())
 
 def predict_for_user_df(ugr_df, optimized_model, username="jomonsugi"):
     client = MongoClient()
@@ -193,14 +221,14 @@ def predict_for_user_df(ugr_df, optimized_model, username="jomonsugi"):
     print(user_df.collect())
 
 def predicted_to_df():
-
+    pass
 
 
 if __name__ == '__main__':
     #-------------------------------------------
     #note that mllib must be used on functions that use the rdd pipeline when #using the ALS library and ml for the DF pipeline
     #-------------------------------------------
-    ugr_df, ugr_rdd = mongo_to_rdd()
+    ugr_df, ugr_rdd = mongo_to_rdd_df()
 
     #---------------------RDD Pipeline----------------------
     #### for optimizing hyperparemeters ON PARTIAL DATA ####
@@ -208,11 +236,11 @@ if __name__ == '__main__':
     #
     # predictions, rates_and_preds = predict_test_rdd(train, validation, test, validation_for_predict, test_for_predict)
     # optimized_model = rdd_predict(ugr_rdd)
-    # predict_for_user_rdd(ugr_rdd, optimized_model, username="ahalm")
+    df_test = predict_for_user_rdd(ugr_rdd, ugr_df, username="ahalm")
 
     #---------------------DF Pipeline----------------------
-    df_train, df_val, df_test = train_val_test_df(ugr_df)
-    evaluator = make_evaluator()
-    optimized_model = predict_test_df(df_train, df_val, evaluator)
-    df_predict(ugr_df, optimized_model)
+    # df_train, df_val, df_test = train_val_test_df(ugr_df)
+    # evaluator = make_evaluator()
+    # optimized_model = predict_test_df(df_train, df_val, evaluator)
+    # df_predict(ugr_df, optimized_model)
     # predict_for_user_df(ugr_df, optimized_model, username="ahalm")
