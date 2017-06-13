4097e28f17f545b40b49c5a92f8c879136fc865a
Micah Shanks <micah.shanks@gmail.com>
2017-06-12 20:10:34 -0600

    added error and topic plot functions

--- nmf_descriptions.py
+++ nmf_descriptions.py
@@ -2,6 +2,7 @@ from sklearn.decomposition import NMF as NMF_sklearn
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
 import pickle
 import numpy as np
+import matplotlib.pylab as plt
 
 STOP = ['player', 'game']
 
@@ -16,25 +17,61 @@ def tfideffed(desc_processed):
     vocabulary = np.array(tfidfvect.get_feature_names())
     return tfidf, vocabulary
 
-def do_nmf(X):
-    nmf = NMF_sklearn(n_components=10, max_iter=100, random_state=34, alpha=0.0, verbose = True)
-    W = nmf.fit_transform(X)
+def do_nmf(V):
+    nmf = NMF_sklearn(n_components=10, max_iter=100, random_state=34, alpha=.01, verbose = True)
+    W = nmf.fit_transform(V)
     H = nmf.components_
     print('reconstruction error:', nmf.reconstruction_err_)
-    #
-    # for i in rand_articles:
-    #     analyze_article(i, contents, web_urls, W, hand_labels)
     return W, H
+
+def do_nmf_loop(V):
+    rec_err_lst = []
+    for c in range(1,100):
+        nmf = NMF_sklearn(n_components=c, max_iter=100, random_state=34, alpha=.01, verbose = True)
+        W = nmf.fit_transform(V)
+        H = nmf.components_
+        print('reconstruction error:', nmf.reconstruction_err_)
+        rec_err_lst.append(nmf.reconstruction_err_)
+    return W, H, rec_err_lst
 
 def topic_labels(H):
     hand_labels = []
     for i, row in enumerate(H):
-        top_twenty = np.argsort(row)[::-1][:20]
+        top_10 = np.argsort(row)[::-1][:10]
+        print(top_10)
         print('topic', i)
-        print('-->', ' '.join(vocabulary[top_twenty]))
+        print('-->', ' '.join(vocabulary[top_10]))
+
+def plot_bar(H, vocabulary):
+    print("Plot highest weighted terms in basis vectors")
+    for i, row in enumerate(H):
+        top10 = np.argsort(row)[::-1][:10]
+        print(top10)
+        val = np.take(row, top10)
+        print(val)
+        plt.figure(i+1)
+        plt.barh(np.arange(10) + .5, val, color="blue", align="center")
+        print((vocabulary[top10]))
+        plt.yticks(np.arange(10) + .5, (vocabulary[top10]))
+        plt.xlabel("Weight")
+        plt.ylabel("Term")
+        plt.title("Highest Weighted Terms in Basis Vector W%d" % (i + 1))
+        plt.grid(True)
+        plt.savefig("plots/documents_basisW%d.png" % (i + 1), bbox_inches="tight")
+
+
+def rec_error_plot(rec_err_lst):
+    import matplotlib.pyplot as plt
+    plt.plot(rec_err_lst)
+    # plt.ylabel('some numbers')
+    plt.savefig("plots/nmf_rec_error.png")
 
 if __name__ == '__main__':
+    plt.close("all")
     desc_processed = un_pickle()
-    X, vocabulary = tfideffed(desc_processed)
-    W, H = do_nmf(X)
+    V, vocabulary = tfideffed(desc_processed)
+    # W, H = do_nmf(V)
+    W, H, rec_err_lst = do_nmf_loop(V)
     topic_labels(H)
+    rec_error_plot(rec_err_lst)
+    # plot_bar(H, vocabulary)
