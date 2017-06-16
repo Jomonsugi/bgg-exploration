5bcda4e6f76bce165a6685734a3fc5a51d2f2b2d
Micah Shanks <micah.shanks@gmail.com>
2017-06-15 21:37:54 -0600

    dictionary of jpegs

--- get_pretty_names.py
+++ get_pretty_names.py
@@ -6,20 +6,22 @@ client = MongoClient()
 coll = client.bgg.game_comments
 
 games = list(coll.distinct('game'))
+print(len(games))
 
 def caps():
     pretty_games = []
     for game in games:
         game = game.replace("-", " ")
-        game = " ".join([string.capitalize(x) for x in game.split()])
+        game = " ".join([x[:1].upper() + x[1:] for x in game.split()])
         pretty_games.append(game)
     return pretty_games
 
 def pretty_to_pickle(pretty_games):
     with open('data/pretty_games_p2.pkl', 'wb') as fp:
-        pickle.dump(pretty_games, fp)
+        pickle.dump(pretty_games, fp, protocol = 2)
 
 
 if __name__ == '__main__':
-    pretty_games = caps()
-    pretty_to_pickle(pretty_games)
+    pass
+    # pretty_games = caps()
+    # pretty_to_pickle(pretty_games)
