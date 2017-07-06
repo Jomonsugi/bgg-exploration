fae454210ba4ec2754acd0b958a4d520c3735582
Micah Shanks <micah.shanks@gmail.com>
2017-07-06 11:39:09 -0600

    updated app for 14K games

--- web_app/content_based_for_flask.py
+++ web_app/content_based_for_flask.py
@@ -15,7 +15,6 @@ def get_data():
 
 def mechanics(df):
     column_lst = list(df.columns.values)
-    print(df.head())
     mechanics = [m for m in column_lst if 'mechanic' in m and m != 'mechanics']
     indexed_df = df.set_index(['game'])
     df_mechs = indexed_df[mechanics]
@@ -25,7 +24,6 @@ def mechanics(df):
 
 def category(df):
     column_lst = list(df.columns.values)
-    # print(column_lst)
     category = [c for c in column_lst if 'category' in c and c != 'categories']
     indexed_df = df.set_index(['game'])
     df_cats = indexed_df[category]
@@ -35,20 +33,20 @@ def category(df):
 
 def hamming(bool_df, df):
     distance_matrix = (1 - pairwise_distances(bool_df, metric = "hamming"))
-    top_10 = list(distance_matrix[0].argsort()[:-10:-1])
+    # top_10 = list(distance_matrix[0].argsort()[:-10:-1])
     # print(df.iloc[top_10,149])
     # print(distance_matrix)
     return distance_matrix
 
 def jaccard(bool_df, df):
     distance_matrix = (1 - pairwise_distances(bool_df, metric = "jaccard"))
-    top_10 = list(distance_matrix[0].argsort()[:-10:-1])
+    # top_10 = list(distance_matrix[0].argsort()[:-10:-1])
     # print(df.iloc[top_10,149])
     return distance_matrix
 
 def prep_columns(df):
-    df = df[['Board Game Rank','game_id','game','description','playing_time','min_players', 'max_players', 'best_num_players', 'avg_rating', 'avg_weight', 'nmf', 'Game']]
-    df.columns = ['BGG Rank','game_id','game','Description','Playing Time','Min Players', 'Max Players', 'Best Num Players', 'Avg Rating', 'Complexity', 'nmf', 'Game']
+    df = df[['Board Game Rank','game_id','game','description','playing_time','min_players', 'max_players', 'best_num_players', 'avg_rating', 'avg_weight']]
+    df.columns = ['BGG Rank','game_id','Game','Description','Playing Time','Min Players', 'Max Players', 'Best Num Players', 'Avg Rating', 'Complexity']
     return df
 
 def un_pickle_labeled_df():
@@ -57,20 +55,22 @@ def un_pickle_labeled_df():
     return nmf_labeled_df
 
 def for_flask_content(board_game, best_num_player, min_time, max_time):
-    nmf_labeled_df = un_pickle_labeled_df()
-    choices = nmf_labeled_df['Game'].tolist()
+    df = get_data()
+    #using fuzzywuzzy to find best match
+    choices = df['game'].tolist()
     board_game = process.extract(board_game, choices, limit=1)
     board_game = board_game[0][0]
-    df = get_data()
-    df['nmf'] = nmf_labeled_df['nmf']
-    df['Game'] = nmf_labeled_df['Game']
+    #creating df with only mechanic and category boolean values
     df_mechs = mechanics(df)
     df_cats = category(df)
     mech_cat_df = pd.concat([df_mechs, df_cats], axis=1)
+    #calculating distance matrix
     distance_matrix = hamming(mech_cat_df, df)
-    idx = int((df.Game[df.Game == board_game].index.tolist())[0])
+    #finding index for board game and filtered list based on distance matrix
+    idx = int((df.game[df.game == board_game].index.tolist())[0])
     sorted_idx = list(distance_matrix[idx].argsort()[::-1])
     sorted_df = df.iloc[sorted_idx,:]
+    #changing column names for app output
     one_user_df = prep_columns(sorted_df)
     #for minimum and maximum time
     if min_time:
@@ -87,14 +87,16 @@ def for_flask_content(board_game, best_num_player, min_time, max_time):
     if 5 in best_num_player:
         one_user_df = one_user_df.loc[(one_user_df['Best Num Players'] > 5) | one_user_df['Best Num Players'].isin(best_num_player)]
         one_user_df = one_user_df.reset_index()
-        print(one_user_df.head())
     else:
         one_user_df = one_user_df.loc[(one_user_df['Best Num Players'].isin(best_num_player))]
         one_user_df = one_user_df.reset_index()
-        print(one_user_df.head())
     one_user_df = one_user_df.round({'Complexity': 1 })
+    #
+    one_user_df['BGG Rank'] = one_user_df['BGG Rank'].fillna(0).astype(int)
+    one_user_df['BGG Rank'] = one_user_df['BGG Rank'].astype(int)
     # rendered_df = one_user_df[['Game','Playing Time','Best Num Players' ,'Complexity']]
     rendered_df = one_user_df[['BGG Rank','Game','Playing Time','Min Players', 'Max Players','Best Num Players','Complexity']]
+    rendered_df = rendered_df.loc[(rendered_df['BGG Rank'] < 3000) & rendered_df['BGG Rank'] != 0]
     return rendered_df.iloc[1:21,:]
 
 if __name__ == '__main__':
